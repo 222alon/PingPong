8482904f6f5489dd84adfa1a7da589c36b1a8195
222alon <46647677+222alon@users.noreply.github.com>
2022-01-20 14:09:19 +0200

    Moved MyContractResolver to seperate file

--- PingPong/PingPongClient/ServerIntaractor.cs
+++ PingPong/PingPongClient/ServerIntaractor.cs
@@ -59,23 +59,8 @@ namespace PingPongClient
 
                 var responsePerson = JsonConvert.DeserializeObject<Person>(answer, settings);
 
-
-
                 Output.WriteLine(responsePerson.ToString());
             }
         }
-    }
-    public class MyContractResolver : DefaultContractResolver
-    {
-        protected override IList<JsonProperty> CreateProperties(Type type, MemberSerialization memberSerialization)
-        {
-            var props = type.GetProperties(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)
-                            .Select(p => base.CreateProperty(p, memberSerialization))
-                        .Union(type.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)
-                                   .Select(f => base.CreateProperty(f, memberSerialization)))
-                        .ToList();
-            props.ForEach(p => { p.Writable = true; p.Readable = true; });
-            return props;
-        } 
     }
 }
