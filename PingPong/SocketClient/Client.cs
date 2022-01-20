74d596e089b73fc9b64cdb8848cc0128057b6042
222alon <46647677+222alon@users.noreply.github.com>
2022-01-20 12:01:19 +0200

    Removed try-catch from Client ctor

--- PingPong/SocketClient/Client.cs
+++ PingPong/SocketClient/Client.cs
@@ -11,36 +11,25 @@ namespace SocketClient
 
         public Client(string ip, int port)
         {
-            try
-            {
-                IPAddress ipAddr = Dns.GetHostEntry(ip).AddressList[0];
+            IPAddress ipAddr = Dns.GetHostEntry(ip).AddressList[0];
 
-                // IPAddress ipAddr = IPAddress.Parse(ip);
-                var localEndPoint = new IPEndPoint(ipAddr, port);
+            // IPAddress ipAddr = IPAddress.Parse(ip);
+            var localEndPoint = new IPEndPoint(ipAddr, port);
 
-                // Creation TCP/IP Socket using
-                // Socket Class Constructor
-                var sender = new Socket(ipAddr.AddressFamily,
-                           SocketType.Stream, ProtocolType.Tcp);
+            // Creation TCP/IP Socket using
+            // Socket Class Constructor
+            var sender = new Socket(ipAddr.AddressFamily,
+                        SocketType.Stream, ProtocolType.Tcp);
 
-                // Connect Socket to the remote
-                // endpoint using method Connect()
-                sender.Connect(localEndPoint);
-                ClientSocket = sender;
-                _handler = new SocketMessageHandler(sender);
-            }
-
-            catch (Exception e)
-            {
-                Console.WriteLine($"Error while creating client socket: {e}");
-                throw;
-            }
+            // Connect Socket to the remote
+            // endpoint using method Connect()
+            sender.Connect(localEndPoint);
+            ClientSocket = sender;
+            _handler = new SocketMessageHandler(sender);
         }
 
         ~Client()
         {
-            // Close Socket using
-            // the method Close()
             ClientSocket.Shutdown(SocketShutdown.Both);
             ClientSocket.Close();
         }
